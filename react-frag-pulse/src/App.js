7cbf52f0631815c0a4e6ae2f48955c3a41f5bdd0
jesus <sandoval.j4@outlook.com>
2023-11-14 19:14:54 -0800

    Created preliminary NavBar.js component

--- react-frag-pulse/src/App.js
+++ react-frag-pulse/src/App.js
@@ -1,12 +1,14 @@
 import Banner from "./components/Banner"
 import LoginForm from "./components/LoginForm";
+import NavBar from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <div className="App container-fluid">
-      Banner
-      <Banner />
-      <LoginForm />
+        <NavBar />
+        <Banner />
+        <LoginForm />
 
     </div>
   );
