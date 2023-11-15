7db91a310c878192f2816c4a9347f3afc71601d1
Jesus Angel Sandoval <114720772+iesus-s@users.noreply.github.com>
2023-11-15 13:01:25 -0800

    All NavBar links are working and individual .js files for each page has been created. Don't touch App.js please.

---
+++ src/components/AdsPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AdsPage extends Component {
+    render() {
+        return (
+            <div>
+                ADS PAGE
+            </div>
+        );
+    }
+}
+
+export default AdsPage;
