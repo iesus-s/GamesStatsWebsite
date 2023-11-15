a4c797fc857b3bad1f08c08e703c8f7ad2d78798
LanceR321 <lmreyes0178@gmail.comgit config --global  user.email lmreyes0178@gmail.comgit config --global  user.email lmreyes0178@gmail.com>
2023-11-14 23:42:05 -0800

    updated about us page

--- react-frag-pulse/src/components/AboutUs.js
+++ react-frag-pulse/src/components/AboutUs.js
@@ -2,14 +2,38 @@
 const AboutUs = () => {
     return (
 
-    <section id="Our-Mission">
-    <div className="container mx-auto justify-center">
-       <h1> Our Mission </h1>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, animi atque dignissimos eveniet exer
-       citationem harum labore minima nulla omnis provident, quis sequi vero, vitae. A animi atque ex magnam obcaecati.</p>
-    </div>
+//    <section id="Our-Mission">
+//    <div className="container mx-auto justify-center">
+//       <h1> Our Mission </h1>
+//       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, animi atque dignissimos eveniet exer
+//       citationem harum labore minima nulla omnis provident, quis sequi vero, vitae. A animi atque ex magnam obcaecati.</p>
+//    </div>
+//
+//    </section>
+
+    <section id="Our-Mission1">
+      <div className = "section1">
+          <div className="container text-center">
+              <div class="row align">
+                  <div class="col">
+                      <h1> Our Mission </h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, animi atque dignissimos eveniet exercitationem harum labore minima nulla omnis provident, quis sequi vero, vitae. A animi atque ex magnam obcaecati.</p>
+                  </div>
+
+                  <div className="col">
+                        <h1> Our Mission </h1>
+
+
+
+
+                  </div>
+              </div>
+          </div>
+
+      </div>
 
     </section>
+
     )
 }
 
