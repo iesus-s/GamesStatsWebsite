98950ebfcd18f24ba296edf21e2064dadc19a596
Miguel <miguel.corral24@gmail.com>
2023-11-26 17:42:04 -0800

    modified:   src/components/ApexFAQ.js 	modified:   src/components/ApexLSOne.js 	modified:   src/components/ApexLSTwo.js 	modified:   src/components/CSGOFAQ.js 	modified:   src/components/CSGOLSOne.js 	modified:   src/components/CSGOLSTwo.js 	modified:   src/components/VALOFAQ.js 	modified:   src/components/VALOLSOne.js 	modified:   src/components/VALOLSTwo.js

--- src/components/ApexLSOne.js
+++ src/components/ApexLSOne.js
@@ -1,33 +1,53 @@
-const ApexLSOne = () => {
-    return (
+import React, { useState } from 'react';
 
-        <section id="guides" className="bg-dark text-light p-5">
-        <div className="container">
-          <div className="row align-items-center justify-content-between">
-            
-            <div className="col-md">
-              <img src={require("../images/WraithPic.jpg")} className="img-fluid" alt=""/>
-            </div>
-        
-            <div className="col-md">
-              <h2>Best Character Of The Season</h2>
-              <p>
-                <h2>Wraith</h2>
-                Stay Mobile: Constantly move and reposition during engagements. Wraith's agility and small hitbox make her a challenging target.
-                 Team Communication: Coordinate portal usage with your squad for strategic flanks or rapid escapes.
-                  Use "Into the Void" Wisely: Master the timing of Wraith's tactical ability to dodge enemy fire or initiate surprise attacks.
-                
-              </p>
-              <a href="#" className="btn btn-light mt-3">
-                <i className="bi bi-chevron-right"></i> See Friends Page
-              </a>
-            </div>
-        
+const ApexLSOne = () => {
+  const [showAdditionalText, setShowAdditionalText] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowAdditionalText((prev) => !prev);
+  };
+
+  return (
+    <section id="guides" className="bg-dark text-light p-5">
+      <div className="container">
+        <div className="row align-items-center justify-content-between">
+
+          <div className="col-md">
+            <img src={require("../images/WraithPic.jpg")} className="img-fluid" alt=""/>
           </div>
+
+          <div className="col-md">
+            <h2>Best Character Of The Season</h2>
+            <p>
+              <h2>Wraith</h2>
+              <strong>Stay Mobile:</strong> Constantly move and reposition during engagements. Wraith's agility and small hitbox make her a challenging target.
+              <br />
+              <strong>Team Communication:</strong> Coordinate portal usage with your squad for strategic flanks or rapid escapes.
+              {showAdditionalText && (
+                <>
+                  <br />
+                  <strong>Use "Into the Void" Wisely:</strong> Master the timing of Wraith's tactical ability to dodge enemy fire or initiate surprise attacks.
+                  <br />
+                  <strong>Additional Wraith Tips:</strong>
+                  <ul>
+                    <li>Experiment with different portal placements to create unexpected flanking opportunities.</li>
+                    <li>Communicate with your team about potential threats, and use "Into the Void" defensively to provide cover during critical moments.</li>
+                    <li>Coordinate ultimates with your team for powerful combinations, such as using Wraith's portal alongside a Bangalore airstrike.</li>
+                    <li>Wraith's passive ability, "Voices from the Void," warns you when danger approaches. Pay attention to these cues for better situational awareness.</li>
+                    <li>Practice using the portal aggressively for rapid repositioning during fights, catching opponents off guard.</li>
+                  </ul>
+                </>
+              )}
+            </p>
+            <button className="btn btn-light mt-3" onClick={handleButtonClick}>
+              <i className="bi bi-chevron-right"></i> {showAdditionalText ? 'Read Less' : 'Read More'}
+            </button>
+          </div>
+
         </div>
-        </section> 
-        
-        )
+      </div>
+    </section>
+  );
 }
 
-export default ApexLSOne
+export default ApexLSOne;
--- src/components/ApexLSTwo.js
+++ src/components/ApexLSTwo.js
@@ -1,34 +1,50 @@
-const ApexLSTwo = () => {
-    return (
-        <section id="learn" className="bg-secondary text-light p-5">
-        <div className="container">
-            <div className="row align-items-center justify-content-between">
-            
-            <div className="col-md">
-                <img src={require("../images/GunsForSale.avif")} className="img-fluid" alt=""/>
-            </div>
-        
-            <div className="col-md">
-                <h2>Build Of The Season</h2>
-                <p>
-                <h3>Flatline & Eva-8</h3>
-                
-                    Flatline Assault Rifle: The Flatline's hard-hitting rounds are perfect for engaging enemies at medium to long range. Equip it with a 2x HCOG Bruiser for precision.
-                
-                    
-                    EVA-8 Auto Shotgun: For close-quarters encounters, the EVA-8 is a reliable choice. Enhance its effectiveness with a Shotgun Bolt for faster fire rate.
-                
-                </p>
-                <a href="#" className="btn btn-light mt-3">
-                <i className="bi bi-chevron-right"></i> See your Stats
-                </a>
-            </div>
-        
-            </div>
-        </div>
-        </section>
+import React, { useState } from 'react';
 
-        )
-    }
-    
-    export default ApexLSTwo
+const ApexLSTwo = () => {
+  const [showAdditionalText, setShowAdditionalText] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowAdditionalText((prev) => !prev);
+  };
+
+  return (
+    <section id="learn" className="bg-secondary text-light p-5">
+      <div className="container">
+        <div className="row align-items-center justify-content-between">
+
+          <div className="col-md">
+            <img src={require("../images/GunsForSale.avif")} className="img-fluid" alt=""/>
+          </div>
+
+          <div className="col-md">
+            <h2>Build Of The Season</h2>
+            <p>
+              <h3>Flatline & Eva-8</h3>
+              <strong>Flatline Assault Rifle:</strong> The Flatline's hard-hitting rounds are perfect for engaging enemies at medium to long range. Equip it with a 2x HCOG Bruiser for precision.
+              <br />
+              <strong>EVA-8 Auto Shotgun:</strong> For close-quarters encounters, the EVA-8 is a reliable choice. Enhance its effectiveness with a Shotgun Bolt for faster fire rate.
+            </p>
+            <button className="btn btn-light mt-3" onClick={handleButtonClick}>
+              <i className="bi bi-chevron-right"></i> {showAdditionalText ? 'Read Less' : 'Read More'}
+            </button>
+            {showAdditionalText && (
+              <div className="mt-3">
+                <strong>Additional Build Tips:</strong>
+                <ul>
+                  <li>Experiment with different attachments such as extended mags and stocks to optimize your weapon handling.</li>
+                  <li>Consider pairing the Flatline with a mid-range optic for improved target acquisition.</li>
+                  <li>Mastering the rhythm of the Flatline's recoil pattern will enhance your accuracy during sustained fire.</li>
+                  <li>For the EVA-8, practice quick weapon swaps to maintain control in fast-paced close-quarters combat.</li>
+                  <li>Adapt your playstyle based on the evolving meta and keep an eye on patch notes for any changes that may impact your build.</li>
+                </ul>
+              </div>
+            )}
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ApexLSTwo;
