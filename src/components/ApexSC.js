34a35cb96f0ffc113dac293cd2b18c976259ee94
Miguel <miguel.corral24@gmail.com>
2023-11-26 16:16:27 -0800

    modified:   src/components/ApexLSThree.js 	modified:   src/components/ApexPW.js 	modified:   src/components/ApexSC.js 	modified:   src/components/CSGOFAQ.js 	modified:   src/components/CSGOLSThree.js 	modified:   src/components/CSGOPW.js 	modified:   src/components/CSGOSC.js 	modified:   src/components/VALOLSThree.js 	modified:   src/components/VALOPW.js 	modified:   src/components/VALOSC.js

--- src/components/ApexSC.js
+++ src/components/ApexSC.js
@@ -1,25 +1,22 @@
 const ApexSC = () => {
   return (
-    
-        
     <section id="learn" className="bg-dark text-light p-5 text-center text-sm-start">
-    <div className="container">
+      <div className="container">
         <div className="d-sm-flex align-item-center justify-content-between">
-            <div>
-                <h1>Become a 
-                    <span className="text-danger">
-                     Apex Predator!
-                    </span>
-                </h1>
-                <p className="lead my-4">Embark on your journey to become an Apex Predator by signing up on Fragpulse today! Our website is your gateway to a wealth of crucial information about Apex Legends, ensuring you stay at the forefront of the game's dynamic landscape. By becoming a member, you gain exclusive access to timely updates on weapon meta, character strategies, and the latest in-game events. Stay in the loop with our comprehensive guides, breaking news, and expert analyses that will sharpen your skills and give you a strategic edge. Fragpulse is more than a platform; it's a community where Apex enthusiasts converge to share insights and discuss tactics. Don't miss out on the opportunity to elevate your Apex Legends experience. Sign up now, and join a thriving community dedicated to mastering the art of the arena!</p>
-                <button className="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#clickformore">Find out how!</button>
-                </div>
-                <img src={require("../images/ApexShowCase.jpg")} className="img-fluid w-50 d-none d-sm-block"></img>
+          <div>
+            <h1>Become a{' '}
+              <span className="text-danger">
+                Apex Predator!
+              </span>
+            </h1>
+            <p className="lead my-4">Embark on your journey to become an Apex Predator by signing up on Fragpulse today! Our website is your gateway to a wealth of crucial information about Apex Legends, ensuring you stay at the forefront of the game's dynamic landscape. By becoming a member, you gain exclusive access to timely updates on weapon meta, character strategies, and the latest in-game events. Stay in the loop with our comprehensive guides, breaking news, and expert analyses that will sharpen your skills and give you a strategic edge. Fragpulse is more than a platform; it's a community where Apex enthusiasts converge to share insights and discuss tactics. Don't miss out on the opportunity to elevate your Apex Legends experience. Sign up now, and join a thriving community dedicated to mastering the art of the arena!</p>
+            <button className="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#clickformore">Find out how!</button>
+          </div>
+          <img src={require("../images/ApexShowCase.jpg")} className="img-fluid w-50 d-none d-sm-block" alt="Apex Legends Showcase"></img>
         </div>
-    </div>
+      </div>
     </section>
-
-  )
+  );
 }
 
-export default ApexSC
+export default ApexSC;
--- src/components/CSGOPW.js
+++ src/components/CSGOPW.js
@@ -1,82 +1,75 @@
 const CSGOPW = () => {
-    return (
-  
-  
-  <section id="potw" className="p-5">
-    <div className="container">
-      <div className="row text-center g-4">
-        
-        <div className="col-md">
-          <div className="card bg-dark text-light">
-            <div className="card-body text-center">
-              <div className="h1 mb-3">
-                <i className="bi bi-person-square"></i>
+  const csWebsiteLink = 'https://www.counter-strike.net';
+
+  return (
+    <section id="potw" className="p-5">
+      <div className="container">
+        <div className="row text-center g-4">
+          
+          <div className="col-md">
+            <div className="card bg-dark text-light">
+              <div className="card-body text-center">
+                <div className="h1 mb-3">
+                  <i className="bi bi-person-square"></i>
+                </div>
+                <h3 className="card-title mb-3">SniperSpecter98</h3>
+                <p className="card-text">
+                  "SniperSpecter98 demonstrated unparalleled precision with the AWP, securing a jaw-dropping 4K with quick flick shots on Dust II. His keen eye and strategic positioning showcased a mastery of long-range engagements that left opponents in awe."
+                </p>
+                <a href={csWebsiteLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Read More</a>
               </div>
-              <h3 className="card-title mb-3">SniperSpecter98</h3>
-              <p className="card-text">
-              "SniperSpecter98 demonstrated unparalleled precision with the AWP, securing a jaw-dropping 4K with quick flick shots on Dust II. His keen eye and strategic positioning showcased a mastery of long-range engagements that left opponents in awe."
-                
-              </p>
-              <a href="#" className="btn btn-primary">Read More</a>
             </div>
           </div>
-        </div>
-        
-        
-        
-        <div className="col-md">
-          <div className="card bg-dark text-light">
-            <div className="card-body text-center">
-              <div className="h1 mb-3">
-                <i className="bi bi-person-square"></i>
+          
+          <div className="col-md">
+            <div className="card bg-dark text-light">
+              <div className="card-body text-center">
+                <div className="h1 mb-3">
+                  <i className="bi bi-person-square"></i>
+                </div>
+                <h3 className="card-title mb-3">BlitzStriker</h3>
+                <p className="card-text">
+                  "BlitzStriker lived up to the name, executing lightning-fast entry frags with the AK-47 on Mirage. In a pivotal round, he single-handedly dismantled the opposition, showcasing exceptional aim and reflexes that turned the tide in favor of his team."
+                </p>
+                <a href={csWebsiteLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Read More</a>
               </div>
-              <h3 className="card-title mb-3">BlitzStriker</h3>
-              <p className="card-text">
-              "BlitzStriker lived up to the name, executing lightning-fast entry frags with the AK-47 on Mirage. In a pivotal round, he single-handedly dismantled the opposition, showcasing exceptional aim and reflexes that turned the tide in favor of his team."
-              </p>
-              <a href="#" className="btn btn-primary">Read More</a>
             </div>
           </div>
-        </div>
-        
-        
-       
-        <div className="col-md">
-          <div className="card bg-dark text-light">
-            <div className="card-body text-center">
-              <div className="h1 mb-3">
-                <i className="bi bi-person-square"></i>
+          
+          <div className="col-md">
+            <div className="card bg-dark text-light">
+              <div className="card-body text-center">
+                <div className="h1 mb-3">
+                  <i className="bi bi-person-square"></i>
+                </div>
+                <h3 className="card-title mb-3">TacticalWarden</h3>
+                <p className="card-text">
+                  "TacticalWarden's game-changing utility usage on Inferno stood out as he perfectly timed incendiary grenades and smoke screens to control chokepoints. His strategic play created opportunities for his team to secure crucial rounds and emerge victorious."
+                </p>
+                <a href={csWebsiteLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Read More</a>
               </div>
-              <h3 className="card-title mb-3">TacticalWarden</h3>
-              <p className="card-text">
-              "TacticalWarden's game-changing utility usage on Inferno stood out as he perfectly timed incendiary grenades and smoke screens to control chokepoints. His strategic play created opportunities for his team to secure crucial rounds and emerge victorious."
-              </p>
-              <a href="#" className="btn btn-primary">Read More</a>
             </div>
           </div>
-        </div>
-  
     
-        <div className="col-md">
-          <div className="card bg-dark text-light">
-            <div className="card-body text-center">
-              <div className="h1 mb-3">
-                <i className="bi bi-person-square"></i>
+          <div className="col-md">
+            <div className="card bg-dark text-light">
+              <div className="card-body text-center">
+                <div className="h1 mb-3">
+                  <i className="bi bi-person-square"></i>
+                </div>
+                <h3 className="card-title mb-3">ClutchViper</h3>
+                <p className="card-text">
+                  "In a nail-biting overtime match on Nuke, ClutchViper lived up to his name with a spectacular 1v3 clutch. His calm composure, precise aim, and clever positioning allowed him to outmaneuver opponents and secure the round, ultimately leading his team to triumph."
+                </p>
+                <a href={csWebsiteLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Read More</a>
               </div>
-              <h3 className="card-title mb-3">ClutchViper</h3>
-              <p className="card-text">
-                "In a nail-biting overtime match on Nuke, ClutchViper lived up to his name with a spectacular 1v3 clutch. His calm composure, precise aim, and clever positioning allowed him to outmaneuver opponents and secure the round, ultimately leading his team to triumph."
-              </p>
-              <a href="#" className="btn btn-primary">Read More</a>
             </div>
           </div>
+    
         </div>
-  
       </div>
-    </div>
-  </section>
-  
-      )
-  }
-  
-  export default CSGOPW
+    </section>
+  );
+}
+
+export default CSGOPW;
