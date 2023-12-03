0d675e055623551b40a8a9911f94759ca2978b61
luis <louieb117@gmail.com>
2023-12-03 14:58:47 -0800

    Initial

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,18 @@
 import LoginForm from "../components/LoginForm"
 import Banner from "../components/Banner"
+import axios from "axios"
+
+const baseURL = "";
 
 const Login = () => {
+  const [post, setPost] = React.useState(null);
+  
+  React.useEffect(() =>{
+    axios.get(baseURL).then((Response))
+
+
+  })
+
   return (
     <div>
         <Banner />
