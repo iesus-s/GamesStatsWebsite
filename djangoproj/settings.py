b5f37cea2e32f231a90fe57dd428f6f27293e299
Luis Bello <lbello5509@sdsu.edu>
2023-12-03 21:48:37 -0800

    Django conversion (#117)
    * oh boy
    
    * improvements

--- djangoproj/settings.py
+++ djangoproj/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+<<<<<<< HEAD
+import os
+=======
+>>>>>>> main
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +58,11 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+<<<<<<< HEAD
+        'DIRS': [os.path.join(BASE_DIR,'reactapp/build')],
+=======
         'DIRS': [],
+>>>>>>> main
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+<<<<<<< HEAD
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'reactapp/build/static')
+]
+=======
+>>>>>>> main
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- djangoproj/urls.py
+++ djangoproj/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+<<<<<<< HEAD
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name='index')
+=======
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+>>>>>>> main
 ]
