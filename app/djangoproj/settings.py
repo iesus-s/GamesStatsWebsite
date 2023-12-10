e067a243a4a8a4be112ec542d6c25c2f983c197f
luis <louieb117@gmail.com>
2023-12-10 09:07:54 -0800

    inijnit

--- app/djangoproj/settings.py
+++ app/djangoproj/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'djangoproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': BASE_DIR / 'db.postgresql',
+        'NAME': 'db.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
