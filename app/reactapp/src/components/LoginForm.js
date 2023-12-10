75cd0fc799137f153f3f132e92c04999f5789120
Miguel <miguel.corral24@gmail.com>
2023-12-09 23:47:16 -0800

    modified:   app/reactapp/src/components/LoginForm.js

--- app/reactapp/src/components/LoginForm.js
+++ app/reactapp/src/components/LoginForm.js
@@ -3,10 +3,12 @@ import ForgotPasswordLink from "./ForgotPasswordLink"
 import LoginButton from "./LoginButton"
 import RememberLogin from "./RememberLogin"
 import SignUpButton from "./SignUpButton"
-import {Container, Row} from "react-bootstrap";
-import React from "react";
+import {Form, Button, Container, Row} from "react-bootstrap";
+import { useState, react } from "react";
 
 const LoginForm = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPass] = useState('')
   return (
     // <!--Sign In-->
       <Container fluid>
@@ -19,20 +21,49 @@ const LoginForm = () => {
                             Sign in to Frag Pulse
                         </h1>
                     </div>
-                    <form>
-                        <EmailAndPassword />
+
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="formEMail" className="mb-3">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control 
+                                type="email"
+                                name='email'
+                                value={email}
+                                onChange={e => setProfileImage(e.target.value)}
+
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formPass">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control 
+                                type="text" 
+                                name='password'
+                                placeholder="password" 
+                                value={password}
+                                onChange={e => setProfileName(e.target.value)}
+                            
+                            />
+                        </Form.Group>
+
                         <div className="form-check max-w-sm mx-auto mb-5  ">
                             <RememberLogin />
                             <ForgotPasswordLink />
                         </div>
-                        <LoginButton />
-                        <div className="max-w-sm mx-auto">
-                        </div>
+
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+
                         <div className="max-w-sm mx-auto">
                             <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"/>
                         </div>
+
                         <SignUpButton />
-                    </form>
+                        
+                    </Form>
+
+
                 </div>
             </div>
         </Row>
